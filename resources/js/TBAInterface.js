004d789586fe96dfb9608c9721ccc93bb29f5d3e
Sean Walberg <sean@ertw.com>
2022-03-12 20:44:20 -0500

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "uQUgMNX6B18F8Sj2WG8KZctaYMGB1z2bvjtXQSFKDfoSZQRPXHyDTYGlAQPe1Nfo";
 
 /**
  * Get list of teams in event
